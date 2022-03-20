273214d7991596dee5f4c52cd3150bcf7b463232
JeancoMontoyaC <92815992+JeanCarloMontoya@users.noreply.github.com>
2022-03-19 20:26:19 -0500

    Create Auto.java

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,36 @@
+package test;
+
+public class Auto {
+	String modelo;
+	int precio;
+	Asiento[] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	void cantidadAsientos
+	
+	String verificarIntegridad() {
+		if (registro==motor.registro) {
+			for(i=0,(i<=asientos.length-1),++i) {
+				if (motor.registro!=asientos[i].registro) {
+					System.out.print("copia");
+					break}
+				else (moto.registro==asientos[(asientos.length-1)].registro){
+					System.out.print("real");}
+			}
+		}
+		else {System.out.print("real");}
+		
+		
+				}
+				}
+			}
+		}
+	}
+	
+	
+	
+
+}
